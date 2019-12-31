8b1d62f8375ad60d1d187d2c2fa6d55bb8ccba93
cicirrr <ciciruan90@gmail.com>
2019-12-31 18:57:32 +0800

    add jsonp

---
+++ src/components/Jsonp.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      JSONP
+    </div>
+</template>
+
+<script>
+// eslint-disable-next-line import/no-extraneous-dependencies
+import jsonp from 'jsonp';
+
+export default {
+  name: 'Jsonp',
+  data() {
+    return {
+      obj: {},
+    };
+  },
+  mounted() {
+    const url = 'https://order.imooc.com/pay/cart?jsonpcallback=jQuery19106595521890337777_1577789592700&jsonp=true&_=1577789592702';
+    jsonp(url, (err, result) => {
+      this.obj = result;
+    });
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
